5ccb9c5841cca7c45b27f4a4551eff6ca4a238b0
Tad Hardesty <tad@platymuus.com>
2015-11-23 02:13:38 -0600

    Implement Error trait for Error

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use std::io::Error as IoError;
+use std::error::Error as StdError;
 use hyper::Error as HyError;
 use serde_json::Error as SjError;
 use websocket::result::WebSocketError as WsError;
@@ -39,5 +40,35 @@ impl From<SjError> for Error {
 impl From<WsError> for Error {
 	fn from(err: WsError) -> Error {
 		Error::WebSocket(err)
+	}
+}
+
+impl ::std::fmt::Display for Error {
+	fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+		write!(f, "Discord error ({})", self.description())
+	}
+}
+
+impl StdError for Error {
+	fn description(&self) -> &str {
+		match *self {
+			Error::Hyper(ref inner) => inner.description(),
+			Error::Json(ref inner) => inner.description(),
+			Error::WebSocket(ref inner) => inner.description(),
+			Error::Io(ref inner) => inner.description(),
+			Error::Decode(..) => "json decode error",
+			Error::Status(_) => "erroneous HTTP status",
+			Error::Other(msg) => msg,
+		}
+	}
+
+	fn cause(&self) -> Option<&StdError> {
+		match *self {
+			Error::Hyper(ref inner) => Some(inner),
+			Error::Json(ref inner) => Some(inner),
+			Error::WebSocket(ref inner) => Some(inner),
+			Error::Io(ref inner) => Some(inner),
+			_ => None,
+		}
 	}
 }
